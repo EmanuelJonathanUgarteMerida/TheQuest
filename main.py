72991f3bbfb1a3a44aaf2addb986a550db0f6ea5
Emanuel J. Ugarte Mérida <emanuelugartemerida@gmail.com>
2021-11-02 22:22:10 +0100

    Inicialización de json sheet image

--- main.py
+++ main.py
@@ -2,14 +2,26 @@ import pygame as pg
 
 import os
 
+from pygame.image import load
+
 from TheQuest import FPS, RESOURCES, SC_HEIGHT, SC_WIDTH
 from TheQuest.entities.space_ship import SpaceShip
+from util import SpriteSheet
 
 clock = pg.time.Clock()
 screen = pg.display.set_mode((SC_WIDTH, SC_HEIGHT))
 bg = pg.image.load(os.path.join(
     RESOURCES, 'images', 'background', 'space_1.jpg'))
 bg_rect = bg.get_rect()
+asteroid_sheet = SpriteSheet('asteroids_sheet.png', 'asteroids_sheet.json')
+
+jsonl = asteroid_sheet.data
+im = jsonl['1']
+image = asteroid_sheet.get_image(
+    im['x'], im['y'], im['height'], im['width'], 1)
+image_rect = image.get_rect()
+image_rect.center = (SC_WIDTH/2, SC_HEIGHT/2)
+
 player = SpaceShip('Jonathan')
 game_over = False
 frames = 0
@@ -31,5 +43,6 @@ while not game_over:
     player.update()
 
     screen.blit(bg, bg_rect)
+    screen.blit(image, image_rect)
     screen.blit(player.image, player.rect)
     pg.display.flip()
